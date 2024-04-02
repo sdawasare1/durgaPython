a8407e4830ae1431c5be783827a8a18a36c5ad05
shubham awasare <sawasare@cdacmumbai.in>
2024-04-03 01:04:08 +0530

    lec_1 commit

---
+++ lec_1/4_class.py
@@ -0,0 +1,12 @@
+# Creating Function in Python and calling - 
+
+def f1():
+    print("Good morning Shubham")
+
+f1()    # Calling an function
+
+
+def f2():
+    print("Good Evening Ramesh")
+
+f2()
