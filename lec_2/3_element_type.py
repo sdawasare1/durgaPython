b8442d17fb2093e146d422df3d02003c29413b67
shubham awasare <sawasare@cdacmumbai.in>
2024-04-03 01:18:07 +0530

    lec_2 commit

---
+++ lec_2/3_element_type.py
@@ -0,0 +1,8 @@
+x = 10 
+print("the type of x is : ",type(x))
+
+y = 10.5
+print("the type of y is : ", type(y))
+
+z = True
+print("the type of z is : ", type(z))
---
+++ lec_2/4_function.py
@@ -0,0 +1,9 @@
+# declaring the function in python
+
+x = 10
+
+def f1():
+    print("The value of x is : ", x)
+
+
+f1() #calling an function
