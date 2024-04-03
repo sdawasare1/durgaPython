607e28ef06a912fa7eb1fb60a785633762254d1c
shubham awasare <sawasare@cdacmumbai.in>
2024-04-03 12:20:24 +0530

    lec_2 commit

---
+++ lec_2/5_random.py
@@ -0,0 +1,12 @@
+from random import * 
+
+# one digit
+print(randint(0,10))
+
+# Five digit
+print(randint(0,10),randint(0,10),randint(0,10),randint(0,10),randint(0,10),sep='')
+
+
+# for 10 random numbers - 
+for i in range(10):
+    print(randint(0,10),randint(0,10),randint(0,10),randint(0,10),randint(0,10),sep='')
