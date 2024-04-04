490c651283c12015b38548109cf2b6f21a75dabd
shubham awasare <sawasare@cdacmumbai.in>
2024-04-04 18:22:45 +0530

    commit lec_4 code

---
+++ lec_4/1_identifiers.py
@@ -0,0 +1,11 @@
+
+# Conversion of values - 
+
+def conversion(number):
+    print("Binary value is : ",bin(number))
+    print("hex value is : ",hex(number))
+    print("oct value is : ",oct(number))
+
+s = int(input("Enter the number : "))
+sc = conversion(s)
+print(sc)
